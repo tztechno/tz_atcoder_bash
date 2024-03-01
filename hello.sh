38d5dbdcd979ee9c2b9ef656c30b4379bd3349cc
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-01 16:54:58 +0900

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# これはコメントです
+echo "Hello, World!"
+
+# 変数の使用例
+name="John"
+echo "My name is $name"
+
+# 条件分岐の例
+if [ $name == "John" ]; then
+  echo "You are John!"
+else
+  echo "You are not John!"
+fi
